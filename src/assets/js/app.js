30dcf109c29ee42455760b7b6fc662c90abb0508
meokeefe <meokeefe@cisco.com>
2018-04-18 16:29:30 -0400

    Autorefresh, favicon, updated widths for columns (#6)
    * Auto refresh except for delete
    
    * page title

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,6 +10,7 @@ import Foundation from 'foundation-sites';
 window.$ = $;
 window.onload = init;
 $(document).foundation();
+setInterval(updateUI, 500); // update every 500 ms
 
 function init() {
   populateTables()
@@ -33,16 +34,24 @@ function populateTables() {
   updateReleasesTable()
 }
 
-var AllClusters  = "";
 
+
+// inline updates on refresh
 
 function updateClusterTable()  {
   $.get("http://172.16.7.101:30900/v0/clusters",
     function(data) {
       data.forEach(function(c) {
-        AllClusters += "  " + c.metadata.name
-        var newrow = '<tr><td>' + c.metadata.name + '</td><td>' + JSON.stringify(c.metadata.labels) + '</td><td>' + '<span class="badge success">✔</span>' + '</td><td>' + c.metadata.annotations.NumPods +  '</td></tr>'
-        $('#clustable tr:last').after(newrow);
+        // if new clusters, make a new row
+        if($("#" + c.metadata.name ).length == 0) {
+          //it doesn't exist
+          var newrow = '<tr id="' + c.metadata.name + '"><td>' + c.metadata.name + '</td><td id="' + c.metadata.name + '-clusterLabels">' + JSON.stringify(c.metadata.labels) + '</td><td>' + '<span class="badge success">✔</span>' + '</td><td id="' + c.metadata.name + '-clusterNumPods">' + c.metadata.annotations.NumPods +  '</td></tr>'
+          $('#clustable tr:last').after(newrow);
+        } else {
+          // exists -- do an inline update of num pods
+          var pods = $('#' + c.metadata.name + "-clusterNumPods")
+          pods.text(c.metadata.annotations.NumPods)
+        }
       });
     })
 }
@@ -51,9 +60,20 @@ function updateReleasesTable() {
   $.get("http://172.16.7.101:30900/v0/releases",
     function(data) {
       data.forEach(function(r) {
-        var newrow = '<tr><td>' + r.Name + '</td><td>' + r.Version + '</td><td>'  + r.Chart.Metadata.Name + '</td><td>' + r.Chart.Metadata.Version + '</td><td>' + r.OnCluster +  '</td></tr>'
+        if($("#" + r.Name + r.OnCluster ).length == 0) {
+        var newrow = '<tr class="releaseRow" id="' + r.Name + r.OnCluster + '"><td>' + r.Name + '</td><td id="' + r.Name + r.OnCluster + '-version">' + r.Version + '</td><td id="' + r.Name + r.OnCluster + '-chartName">'  + r.Chart.Metadata.Name + '</td><td id="' + r.Name + r.OnCluster + '-chartVersion">' + r.Chart.Metadata.Version + '</td><td>' + r.OnCluster +  '</td></tr>'
         $('#releaseTable tr:last').after(newrow);
-      });
+    } else {
+      var releaseV = $('#' + r.Name + r.OnCluster + "-version")
+      releaseV.text(r.Version)
+
+      var chartName = $('#' + r.Name + r.OnCluster + "-chartName")
+      chartName.text(r.Chart.Metadata.Name)
+      
+      var chartV = $('#' + r.Name + r.OnCluster + "-chartVersion")
+      chartV.text(r.Chart.Metadata.Version)
+      }
+        });
     })
 }
 
